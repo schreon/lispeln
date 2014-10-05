e108beb4b48e5505d8b8aad8f1b7d5c8e34c8aa2
schreon <schreon.loeder@gmail.com>
2014-10-05 20:01:23 +0200

    add separate test case for quotes

---
+++ tests/test_quote.py
@@ -0,0 +1,15 @@
+__author__ = 'schreon'
+
+import unittest
+
+
+class QuoteTestCase(unittest.TestCase):
+    """
+    This test case tests the quote-printer.
+    """
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
